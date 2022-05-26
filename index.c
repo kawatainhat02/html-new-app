e4e5bc20de990b73020710cbdc7bd5604798ef64
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2022-05-26 10:14:12 +0700

    Update index.c

--- index.c
+++ index.c
@@ -1,2 +1,14 @@
 int main(int argc, char** argv)
 { int n; int sum=0; cout<<"nhap n: "; cin>>n; for(int i=1;i<=n;i++){ sum+=i; cout<<i<<"+"; } cout<<"="<<sum; return 0; }
+
+#include<stdio.h>  
+void printName();  
+void main ()  
+{  
+    printf("Hello ");  
+    printName();  
+}  
+void printName()  
+{  
+    printf("Javatpoint");  
+}  
